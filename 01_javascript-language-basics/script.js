8b2d660843d9fb7dce344bf4ae8f3590bb5ace15
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-08 12:05:31 +0200

    adds basic-operators, operator-precedence and coding-challenge_1-solution

--- 01_javascript-language-basics/script.js
+++ 01_javascript-language-basics/script.js
@@ -26,7 +26,7 @@ var johnMark = 'Jogn and Mark'*/
  * Variables mutation and type coercion
  **************************************/
 
-var firstName = 'Tajib'
+/*var firstName = 'Tajib'
 var age = 23
 
 console.log(firstName + ' ' + age)
@@ -44,5 +44,64 @@ job = 'driver'
 alert(firstName + ' s a ' + age + ' years old ' + job + '. Is he married? ' + isMarried)
 
 var lastName = prompt('What is his last name?')
-console.log(firstName + ' ' + lastName)
+console.log(firstName + ' ' + lastName)*/
 
+
+/**********************
+ * Operator precedence
+ **********************/
+
+/*
+var now = 2018
+var yearTajib = 1995
+var fullAge = 18
+
+var isFullAge = now - yearTajib >= fullAge
+console.log(isFullAge)
+
+var ageTajib = now - yearTajib
+var ageHasan = 17
+var average = (ageTajib + ageHasan) / 2
+console.log(average)
+
+// Multiple assignments
+var x, y
+x = y = (3 + 5) * 4 - 6
+console.log(x, y)
+
+// More operators
+x *= 2
+console.log(x)
+x += 10
+console.log(x)
+x++
+console.log(x)
+x--
+console.log(x)*/
+
+
+/*********************
+ * CODING CHALLENGE 1
+ ********************/
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true").
+*/
+
+var massMark = 75
+var heightMark = 180
+
+var massJohn = 95
+var heightJohn = 190
+
+bmiMark = massMark / (heightMark * heightMark)
+bmiJohn = massJohn / (heightJohn * heightJohn)
+console.log(bmiMark, bmiJohn)
+
+var information = bmiMark > bmiJohn
+
+console.log('Is Mark\'s BMI higher than John\'s?', information)
