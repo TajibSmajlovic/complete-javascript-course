aa4f10ef7cd0d00c6e0b82fd1327427709b7327b
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-08 11:34:02 +0200

    adds variables-and-data-types and variable-mutation-and-type-coercion

---
+++ 01_javascript-language-basics/script.js
@@ -0,0 +1,48 @@
+/*************************
+* Variables and data types
+**************************/
+
+/*var firstName = 'Tajib'
+console.log(firstName)
+
+var lastName = 'Smajlovic'
+var age = 23
+
+var fullAge = true
+console.log(fullAge)
+
+var job
+console.log(job)
+
+job = 'Teacher'
+console.log(job)
+
+// Variable naming rules
+var $_3years = 3
+var johnMark = 'Jogn and Mark'*/
+
+
+/**************************************
+ * Variables mutation and type coercion
+ **************************************/
+
+var firstName = 'Tajib'
+var age = 23
+
+console.log(firstName + ' ' + age)
+
+var job, isMarried
+job = 'Teacher'
+isMarried = false
+
+console.log(firstName + ' s a ' + age + ' years old ' + job + '. Is he married? ' + isMarried)
+
+// Variable mutation
+age = 'twenty eight'
+job = 'driver'
+
+alert(firstName + ' s a ' + age + ' years old ' + job + '. Is he married? ' + isMarried)
+
+var lastName = prompt('What is his last name?')
+console.log(firstName + ' ' + lastName)
+
