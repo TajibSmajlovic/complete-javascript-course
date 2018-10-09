05b87ce9ec7e8955994810b10e304c85597de8ee
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-09 21:30:58 +0200

    adds if-else-statements, boolean-logic, the-ternary-operator-and-switch-statements, truthy-and-falsy-values-and-equality-operators and coding-challenge_2-solution

--- 01_javascript-language-basics/script.js
+++ 01_javascript-language-basics/script.js
@@ -92,11 +92,12 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true").
 */
 
+/*
 var massMark = 75
-var heightMark = 180
+var heightMark = 1.80
 
 var massJohn = 95
-var heightJohn = 190
+var heightJohn = 1.90
 
 bmiMark = massMark / (heightMark * heightMark)
 bmiJohn = massJohn / (heightJohn * heightJohn)
@@ -104,4 +105,157 @@ console.log(bmiMark, bmiJohn)
 
 var information = bmiMark > bmiJohn
 
-console.log('Is Mark\'s BMI higher than John\'s?', information)
+console.log('Is Mark\'s BMI higher than John\'s?', information)*/
+
+
+/***********************
+ * If 7 else statements
+ **********************/
+
+/*
+var firstName = 'Tajib';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var isMarried = true;
+
+if (isMarried) {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' will hopefully marry soon :)');
+}
+
+var massMark = 75;
+var heightMark = 1.80;
+var massJohn = 95;
+var heightJohn = 1.90;
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+if (BMIMark > BMIJohn) {
+    console.log('Mark\'s BMI is higher than John\'s.');
+} else {
+    console.log('John\'s BMI is higher than Marks\'s.');
+}*/
+
+
+/*****************
+ * Boolean logic
+ *****************/
+
+/*
+var firstName = 'Tajib';
+var age = 23;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man.');
+} else {
+    console.log(firstName + ' is a man.');
+}*/
+
+
+/*********************************************
+ * The ternary Operator and Switch Statements
+ *********************************************/
+
+/*
+var firstName = 'Adnan';
+var age = 23;
+
+// Ternary operator
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+
+console.log(drink);
+
+// Switch statement
+var job = 'instructor';
+switch (job) {
+    case 'teacher':
+    case 'instructor':
+        console.log(firstName + ' teaches kids how to code.');
+        break;
+    case 'driver':
+        console.log(firstName + ' drives an uber in Lisbon.');
+        break;
+    case 'designer':
+        console.log(firstName + ' designs beautiful websites.');
+        break;
+    default:
+        console.log(firstName + ' does something else.');
+}
+
+age = 56;
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >= 20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}*/
+
+
+/*************************************************
+ * Truthy and Falsy values and equality operators
+ *************************************************/
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+/*
+var height;
+height = 23;
+
+if (height || height === 0) {
+    console.log('Variable is defined');
+} else {
+    console.log('Variable has NOT been defined');
+}
+
+// Equality operators
+if (height === '23') {
+    console.log('The == operator does type coercion!');
+}*/
+
+
+/*********************
+ * CODING CHALLENGE 2
+ ********************/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws. */
+
+let teamJohn = (89 + 120 + 103) / 3
+let teamMike = (116 + 94 + 123) / 3
+let teamMary = (97 + 134 + 105) / 3
+console.log(teamJohn, teamMike, teamMary)
+
+if (teamJohn > teamMike && teamJohn > teamMary) {
+    console.log('John\'s team won')
+} else if (teamMike > teamJohn && teamMike > teamMary) {
+    console.log('Mike\'s team won')
+} else if (teamMary > teamMike && teamMary > teamJohn) {
+    console.log('Mary\'s team won')
+} else {
+    console.log('It is draw')
+}
