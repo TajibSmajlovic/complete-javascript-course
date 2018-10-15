1a967d6e6f30fffec4928a18351e41b28a4c84a4
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-15 13:23:09 +0200

    adds handling-rolling-the-dice, updating-score and changing-the-active-player

--- 03_dom-pig-game/app.js
+++ 03_dom-pig-game/app.js
@@ -6,19 +6,64 @@
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game */
 
-let scores, roundScore, activePlayer, dice;
+let scores, roundScore, activePlayer;
 
 scores = [0, 0]
 roundScore = 0
 activePlayer = 0
 
-dice = Math.floor(Math.random() * 6) + 1
-console.log(dice)
-
-document.querySelector(`#current-${activePlayer}`).textContent = dice
-//document.querySelector(`#current-${activePlayer}`).innerHTML = '<em>' + dice +'</em>'
-
-/*let x = document.querySelector('#score-0').textContent
-console.log(x)*/
 
 document.querySelector('.dice').style.display = 'none'
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+
+    // Random number
+    let dice = Math.floor(Math.random() * 6) + 1
+
+    // Display the result
+    let diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block'
+    diceDOM.src =  `img/dice-${dice}.png`
+
+    // Update the round score if the rolled number wan NOT a 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+        roundScore = 0
+
+        document.getElementById('current-0').textContent = '0'
+        document.getElementById('current-1').textContent = '0'
+
+        document.querySelector('.player-0-panel').classList.toggle('active')
+        document.querySelector('.player-1-panel').classList.toggle('active')
+
+        diceDOM.style.display = 'none'
+
+        /*document.querySelector('.player-0-panel').classList.remove('active')
+        document.querySelector('.player-1-panel').classList.add('active')*/
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector(`#current-${activePlayer}`).textContent = dice
+//document.querySelector(`#current-${activePlayer}`).innerHTML = '<em>' + dice +'</em>'
+/*let x = document.querySelector('#score-0').textContent
+console.log(x)*/
