937317e42d78e869c9441affcefb08dc9bb6ed9c
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-17 13:47:09 +0200

    adds hold-button-functionality and new-game-button-functionality

--- 03_dom-pig-game/app.js
+++ 03_dom-pig-game/app.js
@@ -8,20 +8,46 @@
 
 let scores, roundScore, activePlayer;
 
-scores = [0, 0]
-roundScore = 0
-activePlayer = 0
+let init = () => {
+    scores = [0, 0]
+    roundScore = 0
+    activePlayer = 0
+
+    document.querySelector('.dice').style.display = 'none'
+
+    document.getElementById('score-0').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('score-1').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+    document.getElementById(`name-0`) .textContent = 'Player 1'
+    document.getElementById(`name-1`) .textContent = 'Player 2'
+
+    document.querySelector(`.player-0-panel`).classList.remove('winner')
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector(`.player-1-panel`).classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.add('active')
 
 
-document.querySelector('.dice').style.display = 'none'
+}
+let nextPlayer = () => {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+    roundScore = 0
 
-document.getElementById('score-0').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    document.querySelector('.dice').style.display = 'none'
+}
+
+init()
+// New game
+document.querySelector('.btn-new').addEventListener('click', init)
 
 document.querySelector('.btn-roll').addEventListener('click', () => {
-
     // Random number
     let dice = Math.floor(Math.random() * 6) + 1
 
@@ -37,33 +63,22 @@ document.querySelector('.btn-roll').addEventListener('click', () => {
         document.querySelector(`#current-${activePlayer}`).textContent = roundScore
     } else {
         // Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-        roundScore = 0
-
-        document.getElementById('current-0').textContent = '0'
-        document.getElementById('current-1').textContent = '0'
-
-        document.querySelector('.player-0-panel').classList.toggle('active')
-        document.querySelector('.player-1-panel').classList.toggle('active')
-
-        diceDOM.style.display = 'none'
-
-        /*document.querySelector('.player-0-panel').classList.remove('active')
-        document.querySelector('.player-1-panel').classList.add('active')*/
+        nextPlayer()
     }
 })
 
+document.querySelector('.btn-hold').addEventListener('click', () => {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore
+    document.getElementById(`score-${activePlayer}`).textContent = scores[activePlayer]
 
-
-
-
-
-
-
-
-
-
-// document.querySelector(`#current-${activePlayer}`).textContent = dice
-//document.querySelector(`#current-${activePlayer}`).innerHTML = '<em>' + dice +'</em>'
-/*let x = document.querySelector('#score-0').textContent
-console.log(x)*/
+    // Check if player won the game
+    if (scores[activePlayer] >= 100) {
+        document.getElementById(`name-${activePlayer}`) .textContent = 'WINNER'
+        document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner')
+        document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
+        document.querySelector('.dice').style.display = 'none'
+    } else {
+        nextPlayer()
+    }
+})
