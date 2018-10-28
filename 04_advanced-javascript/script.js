0f7a89455842565e2fe322dc0dccc4f2f461a7d8
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-28 16:29:41 +0100

    adds IIFE, closures and coding-challenge_1-solution

--- 04_advanced-javascript/script.js
+++ 04_advanced-javascript/script.js
@@ -115,13 +115,12 @@ let ages = arrayCalc(years, calculateAge)
 let fullAges = arrayCalc(ages, isFullAge)
 let rates = arrayCalc(ages, maxHearthRate)
 
-
 console.log(ages)
 console.log(fullAges)
 console.log(rates)*/
 
 
-// Functions returning functions
+/*// Functions returning functions
 function interviewQuestion(job) {
     if (job === 'designer') {
         return function (name) {
@@ -147,4 +146,120 @@ designerQuestion('Smajke')
 designerQuestion('Hasan')
 designerQuestion('Rejhana')
 
-interviewQuestion('teacher')('Tajib')
+interviewQuestion('teacher')('Tajib')*/
+
+
+/*// Immediately Invoked Functions (IIFE)
+/!*function game() {
+    let score = Math.random() * 10
+    console.log(score >= 5)
+}
+game()*!/
+
+(function () {
+    let score = Math.random() * 10
+    console.log(score >= 5)
+    }) ()
+//console.log(score)
+
+(function (goodLuck) {
+    let score = Math.random() * 10
+    console.log(score >= 5 - goodLuck)
+}) (5)*/
+
+
+/*// Closures
+/!*function retirement(retirementAge) {
+    let a = ' years left until retirement.'
+    return function (yearOfBirth) {
+        let age = 2018 - yearOfBirth
+        console.log((retirementAge - age) + a)
+    }
+}
+
+let retirementUS = retirement(66)(1995)
+let retirementGermany = retirement(65)(1990)
+let retirenmentBosnia = retirement(70)(1963)*!/
+
+/!*function interviewQuestion(job) {
+    let designer = ', can you please explain what UX design is?'
+    let teacher = 'What subject do you teach, '
+
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + designer)
+        } else if (job === 'teacher') {
+            console.log(teacher + name + '?')
+        } else {
+            console.log('Hello ' + name + ', what do you do?')
+        }
+    }
+}
+interviewQuestion('designer')('Tajib')
+interviewQuestion('teacher')('Hasan')
+interviewQuestion('autolimar')('Šukrija')*!/*/
+
+
+/////////////////////////////
+// CODING CHALLENGE
+/*--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+(function () {
+    function Question(question, answers, correct) {
+        this.question = question
+        this.answers = answers
+        this.correct = correct
+    }
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question)
+
+        let i = 0
+        this.answers.forEach((a) => {
+            console.log(`${i}: ${a}`)
+            i++
+        })
+    }
+    Question.prototype.checkAnswer = function (ans) {
+        if (ans === this.correct) {
+            console.log('correct')
+        } else {
+            console.log('wrong')
+        }
+    }
+
+    let q1 = new Question('Is JavaScript the coolest programming language in the world?',
+        ['Yes', 'No'],
+        0);
+
+    let q2 = new Question('What is the name of this course\'s teacher?',
+        ['John', 'Micheal', 'Jonas'],
+        2);
+
+    let q3 = new Question('What does best describe coding?',
+        ['Boring', 'Hard', 'Fun', 'Tediuos'],
+        2);
+
+    let questions = [q1, q2, q3]
+    let n = Math.floor(Math.random() * questions.length);
+    questions[n].displayQuestion()
+
+    let answer = parseInt(prompt('What is your answer?'))
+    questions[n].checkAnswer(answer)
+}())
