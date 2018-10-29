dee741bad50da525648788e6075120898651f06f
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-10-29 14:41:49 +0100

    adds coding-challenge_2-solution

--- 04_advanced-javascript/script.js
+++ 04_advanced-javascript/script.js
@@ -200,9 +200,69 @@ interviewQuestion('teacher')('Hasan')
 interviewQuestion('autolimar')('Šukrija')*!/*/
 
 
-/////////////////////////////
+/*// Bind, call, apply
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+john.presentation('formal', 'morning');
+
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+// Another cool example
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);*/
+
+
+/*/////////////////////////////
 // CODING CHALLENGE
-/*--- Let's build a fun quiz game in the console! ---
+/!*--- Let's build a fun quiz game in the console! ---
 
 1. Build a function constructor called Question to describe a question. A question should include:
 a) question itself
@@ -220,7 +280,7 @@ c) correct answer (I would use a number for this)
 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
-*/
+*!/
 (function () {
     function Question(question, answers, correct) {
         this.question = question
@@ -262,4 +322,82 @@ c) correct answer (I would use a number for this)
 
     let answer = parseInt(prompt('What is your answer?'))
     questions[n].checkAnswer(answer)
+}())*/
+
+
+/*--- Expert level ---
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+11. Display the score in the console. Use yet another method for this.*/
+(function () {
+    function Question(question, answers, correct) {
+        this.question = question
+        this.answers = answers
+        this.correct = correct
+    }
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question)
+
+        let i = 0
+        this.answers.forEach((a) => {
+            console.log(`${i}: ${a}`)
+            i++
+        })
+    }
+    Question.prototype.checkAnswer = function (ans, callback) {
+        let sc
+        if (ans === this.correct) {
+            console.log('correct')
+            sc = callback(true)
+        } else {
+            console.log('wrong')
+            sc = callback(false)
+        }
+
+        this.displayScore(sc)
+    }
+    Question.prototype.displayScore = function(score) {
+        console.log(score)
+        console.log('------------------')
+    }
+
+    let q1 = new Question('Is JavaScript the coolest programming language in the world?',
+        ['Yes', 'No'],
+        0);
+
+    let q2 = new Question('What is the name of this course\'s teacher?',
+        ['John', 'Micheal', 'Jonas'],
+        2);
+
+    let q3 = new Question('What does best describe coding?',
+        ['Boring', 'Hard', 'Fun', 'Tediuos'],
+        2);
+    let questions = [q1, q2, q3]
+
+    function score() {
+        let sc = 0
+        return function (correct) {
+            if (correct) {
+                sc++
+            } else {
+                sc--
+            }
+            return sc
+        }
+    }
+    let keepScore = score()
+
+    function nextQuestion() {
+        let n = Math.floor(Math.random() * questions.length);
+        questions[n].displayQuestion()
+
+        let answer = prompt('What is your answer?')
+
+        if (answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keepScore)
+            nextQuestion()
+        }
+    }
+    nextQuestion()
 }())
