9332fe37620a4a3218ce7b16c48ea2f2780ac4cc
Tajib Smajlović <smajlovic.tajib@gmail.com>
2018-12-17 21:58:39 +0100

    adds search-results-pagination

--- 09_forkify/src/js/index.js
+++ 09_forkify/src/js/index.js
@@ -40,6 +40,15 @@ elements.searchForm.addEventListener('submit', e => {
 
 })
 
+elements.searchResultPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10)
+        searchView.clearResults()
+        searchView.renderResults(state.search.results, goToPage)
+    }
+});
+
 /*const search = new Search('pizza')
 console.log(search)
 search.getResults()*/
