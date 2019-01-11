9723d85b8ba6bbdc98fdadebd6aeb33b70c5ba70
Tajib Smajlović <smajlovic.tajib@gmail.com>
2019-01-11 11:00:17 +0100

    Course is Finished!

--- 09_forkify/src/js/config.js
+++ 09_forkify/src/js/config.js
@@ -1,4 +1,2 @@
-const proxy = 'https://cors-anywhere.herokuapp.com/'
-const key = '6a6fbfa2f18d6ede732300746b3e7f3e'
-
-module.exports = { proxy, key }
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '462b1cc8d4f2730081462fbc65136320';
--- 09_forkify/src/js/models/Search.js
+++ 09_forkify/src/js/models/Search.js
@@ -1,18 +1,18 @@
-import axios from 'axios'
-import { key, proxy } from  '../config'
+import axios from 'axios';
+import { key, proxy } from '../config';
 
 export default class Search {
     constructor(query) {
-        this.query = query
+        this.query = query;
     }
 
     async getResults() {
         try {
-            const result = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
-            this.results = result.data.recipes
-            // console.log(this.results)
-        } catch (e) {
-            alert(e)
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
         }
     }
 }
